02cef7a6f6b18cdaa9da6743ffb95903fcb31fb3
hak.447 <hasifkhan.447@gmail.com>
2026-04-10 01:33:54 +0500

    The IK is now working perfectly

--- kinematics/Inverse/findJointAngles.m
+++ kinematics/Inverse/findJointAngles.m
@@ -1,25 +1,34 @@
 function [configs] = findJointAngles(x,y,z, phi)
     d1 = 13.7; a2 = 10.5; a3 = 10.5; a4 = 11;
-    theta1.a = atan2(y, x);
-    theta1.b = atan2(-y, -x);
-    z_bar = z - d1 - a4 * sin(phi);
+   
+
+
+    theta1pos = atan2(y, x);
+    theta1neg= atan2(-y, -x);
     
     configs = zeros(4,4);
     i = 1;
 
-    for t1 = [theta1.a, theta1.b]
-        x_bar = x/cos(t1) - a4 * cos(phi);
-        y_bar = y/sin(t1) - a4 * cos(phi);
-        u = (x_bar^2 + z_bar^2 -a2^2 - a3^2) / (2 * a2 * a3);
-        denom = (a2^2 + 2 * a2 * a3 * u + a3^2);
+    for theta1 = [theta1pos, theta1neg]
+        if (theta1 == theta1pos)
+            r = sqrt(x^2 + y^2);
+        end
+        if (theta1 == theta1neg)
+            r = -sqrt(x^2 + y^2);
+
+        end
+        s = z - d1;
+
+        r_bar = r - a4 * cos(phi);
+        s_bar = s - a4 * sin(phi);
+
+        theta3pos = acos((r_bar^2 + s_bar^2 - a2^2 - a3^2)/(2 * a2 * a3));
         
-        for sign = [1,-1]
-            t3 = atan2(sign * sqrt(1-u^2), u);
-            costh2 = (y_bar * (a2 + a3 * u) + sign * z_bar * (a3 * sqrt(1 - u^2))) / denom;
-            sinth2 = (z_bar * (a2 + a3 * u) - sign * y_bar * (a3 * sqrt(1 - u^2))) / denom;
-            t2 = atan2(sinth2, costh2);
-            t4 = phi - t3 - t2;
-            configs(i,:) = [t1, t2, t3, t4];
+        for theta3 = [theta3pos,-theta3pos]
+            theta2 = atan2(s_bar, r_bar) - atan2(a3 * sin(theta3), a2 + a3*cos(theta3));
+
+            theta4 = phi - theta3 - theta2;
+            configs(i,:) = [theta1, theta2, theta3, theta4];
             i = i+ 1;
         end
     end
