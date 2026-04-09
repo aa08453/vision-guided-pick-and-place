59a3963af9ebeb0cbe536448467dd2845a05036e
hak.447 <hasifkhan.447@gmail.com>
2026-04-10 00:16:14 +0500

    Made fix for inverse kinematics

--- kinematics/Inverse/findJointAngles.m
+++ kinematics/Inverse/findJointAngles.m
@@ -1,38 +1,27 @@
-function [theta1, theta2, theta3, theta4] = findJointAngles(x,y,z, phi)
+function [configs] = findJointAngles(x,y,z, phi)
     d1 = 13.7; a2 = 10.5; a3 = 10.5; a4 = 11;
     theta1.a = atan2(y, x);
     theta1.b = atan2(-y, -x);
-
-    x_bar = x/cos(theta1.a) - a4 * cos(phi);
-    y_bar = y/sin(theta1.a) - a4 * cos(phi);
     z_bar = z - d1 - a4 * sin(phi);
+    
+    configs = zeros(4,4);
+    i = 1;
 
-    u = (x_bar^2 + z_bar^2 -a2^2 - a3^2) / (2 * a2 * a3);
+    for t1 = [theta1.a, theta1.b]
+        x_bar = x/cos(t1) - a4 * cos(phi);
+        y_bar = y/sin(t1) - a4 * cos(phi);
+        u = (x_bar^2 + z_bar^2 -a2^2 - a3^2) / (2 * a2 * a3);
+        denom = (a2^2 + 2 * a2 * a3 * u + a3^2);
+        
+        for sign = [1,-1]
+            t3 = atan2(sign * sqrt(1-u^2), u);
+            costh2 = (y_bar * (a2 + a3 * u) + sign * z_bar * (a3 * sqrt(1 - u^2))) / denom;
+            sinth2 = (z_bar * (a2 + a3 * u) - sign * y_bar * (a3 * sqrt(1 - u^2))) / denom;
+            t2 = atan2(sinth2, costh2);
+            t4 = phi - t3 - t2;
+            configs(i,:) = [t1, t2, t3, t4];
+            i = i+ 1;
+        end
+    end
 
-    theta3.a = atan2(sqrt(1-u^2), u);
-    theta3.b = atan2(-sqrt(1-u^2), u);
-
-   
-    denom = (a2^2 + 2 * a2 * a3 * u + a3^2);
-    costh2.a = y_bar * (a2 + a3 * u) + z_bar * (a3 * sqrt(1 - u^2));
-    sinth2.a = z_bar * (a2 + a3 * u) - y_bar * (a3 * sqrt(1 - u^2));
-
-    costh2.b = y_bar * (a2 + a3 * u) - z_bar * (a3 * sqrt(1 - u^2));
-    sinth2.b = z_bar * (a2 + a3 * u) + y_bar * (a3 * sqrt(1 - u^2));
-
-    costh2.a = costh2.a/denom;
-    sinth2.a = sinth2.a/denom;
-
-    costh2.b = costh2.b/denom;
-    sinth2.b = sinth2.b/denom;
-
-
-    theta2.a = atan2(sinth2.a, costh2.a);
-    theta2.b = atan2(sinth2.b, costh2.b);
-
-    theta4.a = phi - theta3.a - theta2.a;
-    theta4.b = phi - theta3.b - theta2.b;
-
-
-
-end 
+end
